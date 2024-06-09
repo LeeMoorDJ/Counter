8164dad414c3bac84246fac11ca31dff4e5edcab
LeeMoorDJ <anokhin1989@yandex.ru>
2024-06-09 16:50:18 +0300

    закончил Расширенное задание

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var counterButton: UIButton!
     @IBOutlet weak var counterValue: UILabel!
-    
+    @IBOutlet weak var removeCountButton: UIButton!
+    @IBOutlet weak var clearCountButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        counterButton.tintColor = .red
+        removeCountButton.tintColor = .blue
+        clearCountButton.tintColor = .darkGray
     }
     
     
@@ -27,9 +32,34 @@ class ViewController: UIViewController {
         
         print(counter)
         
-        counterValue.text = "Значение счётчика :  \(counter)"
+        counterValue.text = "Значение счётчика : \(counter)"
         
     }
+    
+    
+    @IBAction func removeValueFromCounter(_ sender: UIButton) {
+        print("Удалить 1")
+        
+        if counter > 0 {
+            counter -= 1
+        }
+        
+        print(counter)
+        
+        counterValue.text = "Значение счётчика : \(counter)"
+    }
+    
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        print("Очистить счётчик")
+        
+        counter = 0
+        
+        print(counter)
+        
+        counterValue.text = "Значение счётчика : \(counter)"
+    }
+    
     
     private var counter = 0
 
